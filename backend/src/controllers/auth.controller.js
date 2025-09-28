17bcee5885edde48e3dde47f70dc5d3ace665d23
rafifsuci <rafifnyantuy@gmail.com>
2025-09-28 11:56:38 +0700

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,8 +34,13 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      //before
+      // generateToken(newUser._id, res);
+      // await newUser.save();
+
+      //after
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
